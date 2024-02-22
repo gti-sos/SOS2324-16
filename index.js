1807ad4390ec5fb9693ba0c4c78dfdf32f972351
PABLO SUAREZ <pabsuasar@alum.us.es>
2024-02-22 12:43:23 +0100

    F03 actualizado

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ let app = express();
 const PORT = (process.env.PORT || 10000);
 
 app.use("/", express.static("./public"));
+app.use("/samples/PSS", express.static("./samples/PSS"));
+app.use("/samples/PRR", express.static("./samples/PRR"));
+app.use("/samples/DMC", express.static("./samples/DMC"));
 
 app.get("/cool", (req,res) => {
     res.send(`<html><body><h1>${cool()}</html></body></h1>`);
