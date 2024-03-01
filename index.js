4d2fd9be4c4af872b09ff4bb05b2aff804e1adeb
Pablo Ruiz Rivas <pabruiriv@alum.us.es>
2024-03-01 18:50:25 +0100

    Todos los metodos de la parte azul

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 let cool = require("cool-ascii-faces");
 let express = require("express");
 let bodyParser = require("body-parser");
+let dataStore = require("nedb");
 
 let PSS = require('./index-PSS');
 let PRR = require('./index-PRR');
 let DMC = require('./index-DMC');
 
 let app = express();
+
+let dbRugby = new dataStore();
+let dbVolleyball = new dataStore();
+let dbFootball = new dataStore();
 
 //Variables constantes
 const PORT = (process.env.PORT || 10000);
@@ -33,28 +38,81 @@ app.get("/samples/DMC",(req,res)=>{
     res.send(`<html><body><h1> ${DMC.todosPaises(DMC.datos)}  </h1></body></html>`)
 });
 
-//Recurso para /api de PABLO RIVAS jaja brr 
+//Recurso para /api de PABLO RIVAS 
+app.get(API_BASE+"/stats-rugby/loadInitialData", (req,res) => {
+    dbRugby.find({}, (err, doc) => {
+        if(err){
+            res.sendStatus(500, "Internal Error");
+        }else {
+            if (docs.length === 0) {
+                dbRugby.insert(PRR.array);
+                res.sendStatus(201, "Array created");
+            } else{
+                res.sendStatus(409, "Conflict");
+            }
+        }
+    });
+});
 app.get(API_BASE+"/stats-rugby", (req,res) => {
-    res.send(JSON.stringify(PRR.array));
+    dbRugby.find({}, (err,info) => {
+        if(err){
+            res.sendStatus(500,"Internal Error");
+        }else{
+            res.send(JSON.stringify(info.map((c)=> {
+                delete c._id;
+                return c;
+            })));
+        }
+    });
 });
 app.post(API_BASE+"/stats-rugby", (req,res) => {
-    let stat = req.body;
-    if(PRR.array.includes === stat){
-        res.sendStatus(409, "Conflict");
-    }else if(stat){
-        PRR.array.push(stat);
-        res.sendStatus(201, "Created");
-    }else{
-        res.sendStatus(400, "Bad request");
-    }
-})
-app.delete(API_BASE+"/stats-rugby", (req,res) => {
-    PRR.array.splice(0,PRR.array.length);
-    res.sendStatus(200, "OK");
-})
+    let stat=req.body;
+    dbRugby.insert(stat);
+    res.sendStatus(201,"Created");
+});
 app.put(API_BASE+"/stats-rugby", (req,res) => {
     res.sendStatus(405, "Method Not Allowed");
-})
+});
+app.delete(API_BASE+"/stats-rugby", (req,res) => {
+    dbRugby.remove({},{ multi: true},(err,numRemoved) => {
+        if(err){
+            res.sendStatus(500,"Internal Error");
+        }else{
+            if(numRemoved>=1){
+                res.sendStatus(200,"All removed");
+            }else{
+                res.sendStatus(404,"Not found");
+            }
+        }
+    })
+});
+
+app.get(API_BASE+"/stats-rugby/:name", (req,res) => {
+    let name=req.params.name;
+    dbRugby.find({"name":name}, (err,info) => {
+        if(err){
+            res.sendStatus(404,"Not Found");
+        }else{
+            res.send(JSON.stringify(info));
+        }
+    });
+});
+
+app.delete(API_BASE+"/stats-rugby/:name", (req,res) => {
+    let name=req.params.name;
+    dbRugby.remove( {"name":name},{},(err,numRemoved)=>{
+    if(err){
+        res.sendStatus(500,"Internal Error");
+    }else{
+        if(numRemoved>=1){
+            res.sendStatus(200,"Removed");
+        }else{
+            res.sendStatus(404,"Not found");
+        }
+    }
+    });
+});
+
 
 app.get(API_BASE+"/stats-rugby/loadInitialData", (req,res) => {
     if(PRR.array.length === 0){
@@ -62,18 +120,6 @@ app.get(API_BASE+"/stats-rugby/loadInitialData", (req,res) => {
     }
     res.sendStatus(201, "Array created");
 });
-
-// app.get(API_BASE+"/stats-rugby/:id", (req,res) => {
-//     const parametro = req.params.id;
-//     const playerData = PRR.array.find(x => x.bplace === parametro);
-//     if (playerData) {
-//         res.send(JSON.stringify(playerData)); 
-//     } else {
-//         res.sendStatus(404, "Player not found");
-//     }
-// });
-
-
 
 
 
