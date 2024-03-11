e93d5b0a10157c48d0ea931235530b66c2bab9b6
unknown <domingomorcal@gmail.com>
2024-03-11 15:58:35 +0100

    400

--- api/apiDMC/apiDMC.js
+++ api/apiDMC/apiDMC.js
@@ -312,20 +312,34 @@ app.post(API_BASE+"/stats-volleyball/:nationality", (req,res) => {
 app.put(API_BASE+"/stats-volleyball/:nationality", (req,res) => {
     let nationality  = req.params.nationality;
     const nuevo = req.body;
+    let v_id;
 
-    dbVolleyball.find()
-
-    dbVolleyball.update({"nationality":nationality},{$set: nuevo},(err,numUpdated)=>{
+    dbVolleyball.find({"nationality":nationality},(err,info)=>{
         if (err) {
             res.sendStatus(500, "Internal Error");
         } else {
-            if (numUpdated === 0) {
-                res.sendStatus(404, "Not found");
-            } else {
-                res.sendStatus(200, "Ok");
+            v_id=info[0]._id;
+            if(!(nuevo._id) || nuevo._id!==v_id){
+                res.sendStatus(400,"Bad Request");
+            }else{
+                dbVolleyball.update({"nationality":nationality},{$set: nuevo},(err,numUpdated)=>{
+                if (err) {
+                    res.sendStatus(500, "Internal Error");
+                }else {
+                    if (numUpdated === 0) {
+                        res.sendStatus(404, "Not found");
+                    } else {
+                        res.sendStatus(200, "Ok");
+                    }
+                }
+            });
             }
+            
+            
         }
     });
+
+    
 });
 
 app.delete(API_BASE+"/stats-volleyball/:nationality", (req,res) => {
