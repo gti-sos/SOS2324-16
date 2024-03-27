95f6d5a0189e7235c373865b71ed963b5dc25fcc
Pablo Ruiz Rivas <pabruiriv@alum.us.es>
2024-03-27 14:19:30 +0100

    miercoles de dolores

--- front/src/routes/stats-rugby/[bplace]/[weight]/+page.svelte
+++ front/src/routes/stats-rugby/[bplace]/[weight]/+page.svelte
@@ -10,35 +10,137 @@
     import {page} from '$app/stores';
     let pais=$page.params.bplace;
     let peso=$page.params.weight;
-    let jElegida=[];
+    let jElegido={};
+    let errorMsg="";
 
     onMount(()=>{
-        getJugadoraElegida();
+        getJugadorElegido(pais,peso);
     })
 
-    async function getJugadoraElegida(){
-        let response=await fetch(API,{
+    async function getJugadorElegido(pais,peso){
+        let response=await fetch(API+"/"+pais+"/"+peso,{
                             method:"GET"
-                        })
+                        });
 
         let data=await response.json();
-        data.forEach((j) => {
-            console.log(j.bplace);
-            console.log(pais);
-            if(j.bplace==pais ){
-                jElegida.push(j);
+        jElegido=data;
+        console.log(jElegido);
+    }
+
+    async function putRugby(){
+        try{
+            let response=await fetch(API+"/"+pais+"/"+peso,{
+                                method:"PUT",
+                                headers:{
+                                    "Content-Type":"application/json"
+                                },
+                                body:JSON.stringify(jElegido,null,2)
+                            });
+            let status=await response.status;
+            console.log(`Creation response status ${status}`);
+            if(status===200){
+                getJugadorElegido(jElegido.bplace,jElegido.weight);
+            }else{
+                errorMsg="code: "+status;
             }
-            
-        });
-        
+        }catch(e){
+            errorMsg=e;
+        }
     }
 
 
+
+
 </script>
-Es {jElegida[0]}
-<!-- <ul>
-    {#each jElegida as volleyball_jj}
-        <li><p>Nombre: {volleyball_jj.name}</p> </li>
-        
-    {/each}
-</ul> -->
+Es {jElegido}
+
+<table>
+    <thead>
+        <tr>
+            <th>
+                Equipo
+            </th>
+            <th>
+                Posición
+            </th>
+            <th>
+                Edad
+            </th>
+            <th>
+                Altura (en cm)
+            </th>
+            <th>
+                Peso (en kg)
+            </th>
+        </tr>
+
+    </thead>
+    <tbody>
+        <tr>
+            <td>
+                <input bind:value={jElegido.team}>
+            </td>
+            <td>
+                <input bind:value={jElegido.plabel}>
+            </td>
+            <td>
+                <input bind:value={jElegido.age}>
+            </td>
+            <td>
+                <input bind:value={jElegido.height}>
+            </td>
+            <td>
+                <input bind:value={jElegido.weight}>
+            </td>
+        </tr>
+
+    </tbody>
+    <thead>
+        <tr>
+            <th>
+                Nacionalidad
+            </th>
+            <th>
+                Fecha de nacimiento
+            </th>
+            <th>
+                Apellido
+            </th>
+            <th>
+                Nombre
+            </th>
+            <th>
+                Premios
+            </th>
+
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>
+                <input bind:value={jElegido.bplace}>
+            </td>
+            <td>
+                <input bind:value={jElegido.bdate}>
+            </td>
+            <td>
+                <input bind:value={jElegido.last}>
+            </td>
+            <td>
+                <input bind:value={jElegido.first}>
+            </td>
+            <td>
+                <input bind:value={jElegido.caps}>
+            </td>
+
+        </tr>
+    </tbody>
+
+</table>
+
+<button on:click="{putRugby}">Guardar cambios</button>
+
+{#if errorMsg!=""}
+<hr>
+ERROR: {errorMsg}    
+{/if}
