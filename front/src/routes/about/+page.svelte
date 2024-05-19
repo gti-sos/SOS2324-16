a4ac297f5efb221c0960abc6c021069af36c286b
Pablo Suárez <pabsuasar@alum.us.es>
2024-05-19 10:43:09 +0200

    FIN

--- front/src/routes/about/+page.svelte
+++ front/src/routes/about/+page.svelte
@@ -1,15 +1,23 @@
-<h2>Video Pablo Ruiz Rivas</h2>
+<h2>Vídeo Pablo Ruiz Rivas</h2>
 <ul>
     <li>
-        <a href="https://youtu.be/gWdQBjfA8L0">Enlace del video via YouTube</a>
+        <a href="https://youtu.be/gWdQBjfA8L0">Enlace del vídeo vía YouTube</a>
 
     </li>
 </ul>
 
-<h2>Video Domingo Morales Calderón</h2>
+<h2>Vídeo Domingo Morales Calderón</h2>
 <ul>
     <li>
-        <a href="https://youtu.be/QZ0Typ1SGu0">Enlace del video via YouTube</a>
+        <a href="https://youtu.be/QZ0Typ1SGu0">Enlace del vídeo vía YouTube</a>
+
+    </li>
+</ul>
+
+<h2>Vídeo Pablo Suárez Sarmiento</h2>
+<ul>
+    <li>
+        <a href="https://youtu.be/hhG04Q3U0SE">Enlace del vídeo vía YouTube</a>
 
     </li>
 </ul>
